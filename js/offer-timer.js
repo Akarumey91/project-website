44ad0c7e4cb676dc849191b32acee5321cabf9bc
Akarumey91 <siriusone91@gmail.com>
2020-09-23 23:18:15 +0300

    Made timer

--- js/offer-timer.js
+++ js/offer-timer.js
@@ -1,55 +1,37 @@
-const displayTimer = document.querySelector(".offer-timer");
+const timerEnd = document.getElementById('timer');
+const daysShow = document.getElementById('days');
+const hoursShow = document.getElementById('hours');
+const minutesShow = document.getElementById('minutes');
+const secondsShow = document.getElementById('seconds');
 
-const todayDate = new Date("2020-07-29T00:00:00");
-const endDate = new Date("2020-07-30T00:00:00");
-const leftDate = endDate - todayDate;
-// const leftTime (date) => `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+const endDate = {
+	year: '2020',
+	month: '09',
+	day: '26',
+	hours: '20',
+	minutes: '20',
+	seconds: '00',
+};
 
-// function clock() {
-// 	let time = new Date(),
-// 		hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours(),
-// 		minutes =
-// 			time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes(),
-// 		seconds =
-// 			time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
-// 	document.getElementById("timer").innerHTML =
-// 		hours + ":" + minutes + ":" + seconds;
-// }
-// setInterval(clock, 1000);
+const endDateInString = `${endDate.year}-${endDate.month}-${endDate.day}T${endDate.hours}:${endDate.minutes}:${endDate.seconds}`;
 
-document.getElementById(
-	"test"
-).innerHTML = `${todayDate.getHours()} : ${todayDate.getMinutes()} : ${todayDate.getSeconds()}`;
+setInterval(function () {
+	const now = new Date();
+	const endDate = new Date(endDateInString);
+	const timeLeft = endDate - now;
+	const timeLeftInString = new Date(timeLeft);
+	const days = timeLeftInString.getUTCDate() - 1;
+	const hours = timeLeftInString.getUTCHours();
+	const minutes = timeLeftInString.getUTCMinutes();
+	const seconds = timeLeftInString.getUTCSeconds();
 
-// leftDate / 1000 / 24 / 60 / 60;
-function leftTime(date) {
-	// let days = date / 24 / 60 / 60 / 1000;
-	// let hours = days * 24;
-	// let minutes = hours * 2.5;
-	// let seconds = minutes;
-	// return `${Math.trunc(days)} : ${hours} : ${minutes} : ${seconds}`;
-	let miliseconds = date;
-	let seconds = 0;
-	let minutes = 0;
-	let hours = 0;
-	let days = 0;
-	if ((miliseconds = 1000)) {
-		miliseconds -= 1000;
-		seconds++;
+	if (timeLeft <= 0) {
+		clearInterval(timer);
+		timerEnd.innerHTML = 'Время закончилось';
+	} else {
+		daysShow.innerHTML = `${days < 10 ? '0' + days : days}`;
+		hoursShow.innerHTML = `${hours < 10 ? '0' + hours : hours}`;
+		minutesShow.innerHTML = `${minutes < 10 ? '0' + minutes : minutes}`;
+		secondsShow.innerHTML = `${seconds < 10 ? '0' + seconds : seconds}`;
 	}
-	if ((seconds = 60)) {
-		seconds -= 60;
-		minutes++;
-	}
-	if ((minutes = 60)) {
-		minutes -= 60;
-		hours++;
-	}
-	if ((hours = 24)) {
-		hours -= 24;
-		days++;
-	}
-	console.log(`${days} : ${hours} : ${minutes} : ${seconds}`);
-}
-console.log(leftTime(leftDate));
-console.log(leftDate);
+}, 1000);
